e6f1d23adf3ce9622c3708771f2b33f5b52d8b40
SADHAV S <sadhavpooja@gmail.com>
2025-03-11 20:01:31 +0530

    initial commit

---
+++ web/regression/javascript/fake_current_user.js
@@ -0,0 +1,13 @@
+//////////////////////////////////////////////////////////////////////////
+//
+// pgAdmin 4 - PostgreSQL Tools
+//
+// Copyright (C) 2013 - 2025, The pgAdmin Development Team
+// This software is released under the PostgreSQL Licence
+//
+//////////////////////////////////////////////////////////////////////////
+
+module.exports = {
+  'id': 'pgadmin4@pgadmin.org',
+  'current_auth_source': 'internal'
+};
